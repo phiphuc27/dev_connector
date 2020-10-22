5974c7ec9edfcb50301d603d06ede6632be5dc43
Huỳnh Phi Phúc <34578020+phiphuc27@users.noreply.github.com>
2020-10-22 22:57:30 +0700

    Get logged in user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,9 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current user profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await (
+            await Profile.findOne({ user: req.user.id })
+        ).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res
+                .status(400)
+                .json({ msg: 'There is no profile for this user' });
+        }
+        return res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
